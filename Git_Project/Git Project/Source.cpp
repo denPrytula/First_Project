f1c6723cc5b9257c49dea73ef3515086f69e7c1c
denPrytula <denispritula1010@gmail.com>
2023-06-07 17:06:30 +0300

    First

---
+++ Git_Project/Git Project/Source.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cmath>
+#include <string.h>
+#include <string>
+#include <time.h>
+
+#include <malloc.h>
+using namespace std;
+
+
+
+int main(){
+
+
+
+
+	return 0;
+}
