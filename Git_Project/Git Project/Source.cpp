ca4f8119558d2eb0a46ae41f9ea3a9db76e9acee
denPrytula <denispritula1010@gmail.com>
2023-06-08 14:01:05 +0300

    nothing

--- Git_Project/Git Project/Source.cpp
+++ Git_Project/Git Project/Source.cpp
@@ -1,3 +1,4 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <cmath>
 #include <string.h>
@@ -5,18 +6,39 @@
 #include <time.h>
 #include <malloc.h>
 #include "Git_header.h"
-using namespace std;
+#include <cstdio>
 
+using namespace std;
+const char* filepath = "E:\\file.txt";
 
 
 int main() {
 	srand(time(NULL));
-	int* arr = new int;
+    
+    FILE* f = fopen(filepath, "r");
+    
+
+    int charCount[256] = {0};
+
+    char ch;
+    while ((ch = fgetc(f)) != EOF) {
+        charCount[ch]++;
+    }
+    
+
+    int max = 0;
+    char mostFrequently;
+    for (int i = 0;i < 256;i++) {
+        if (charCount[i] > max) {
+            mostFrequently = i;
+            max = charCount[i];
+        }
+    }
+    
+
+    printf("Char: '%c' meets %d", mostFrequently, max);
+
+    fclose(f);
 	
-	Create_Array(arr, 20, 0, 2);
-	PrintArray(arr, 20);
-
-
-
 	return 0;
 }
